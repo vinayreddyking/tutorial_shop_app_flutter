38abe8e85d4449116a3f69706b0a2fa30773025c
vinayreddyking <vinayreddyking@gmail.com>
2020-05-12 07:44:22 +0530

    I Want to Start From Beggining

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
-
-import './screens/products_overview_screen.dart';
-import './screens/products_detail_screen.dart';
-import './providers/products.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
-      child: MaterialApp(
-        title: 'My Shop',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        debugShowCheckedModeBanner: false,
-        // home: MyHomePage(),
-        initialRoute: '/',
-        routes: {
-          '/': (ctx) => ProductsOverviewScreen(),
-          ProductsDetailScreen.routeName: (ctx) => ProductsDetailScreen(),
-        },
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
